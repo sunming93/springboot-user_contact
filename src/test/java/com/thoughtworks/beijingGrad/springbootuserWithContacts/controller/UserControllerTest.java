0eda71cdf4e487b9c4ec3c87900d27681fabe5c4
mingwhsun <mingwhsun@thoughtworks.com>
2018-09-21 00:34:01 +0800

    [sunming] Add the test should_create_a_contact_for_the_user_with_id_5

--- src/test/java/com/thoughtworks/beijingGrad/springbootuserWithContacts/controller/UserControllerTest.java
+++ src/test/java/com/thoughtworks/beijingGrad/springbootuserWithContacts/controller/UserControllerTest.java
@@ -1,11 +1,17 @@
 package com.thoughtworks.beijingGrad.springbootuserWithContacts.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.beijingGrad.springbootuserWithContacts.domain.Contact;
+import com.thoughtworks.beijingGrad.springbootuserWithContacts.domain.Gender;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
@@ -27,5 +33,18 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$[0].name").value("wu qian"))
                 .andExpect(jsonPath("$[1].id").value(2))
                 .andExpect(jsonPath("$[1].name").value("xin kuan"));
+    }
+
+    @Test
+    void should_create_a_contact_for_the_user_with_id_5() throws Exception {
+        Contact contact = new Contact(3, "zeng zhipeng", 1234567890L, 20, Gender.Male);
+
+        mockMvc.perform(post("/users/5/contacts")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(contact)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$",hasSize(3)))
+                .andExpect(jsonPath("$[2].id").value(3))
+                .andExpect(jsonPath("$[2].name").value("zeng zhipeng"));
     }
 }
